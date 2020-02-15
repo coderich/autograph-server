274a64062b35fe3509622f139b9b4d6da6f3245d
Richard Livolsi <richard.livolsi@goziohealth.com>
2020-02-15 12:22:41 -0500

    Exporting autograph Schema Resolver

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { Schema, Resolver } = require('@coderich/autograph');
 const { createGraphSchema } = require('./src/service/schema.service');
 
 module.exports = {
+  Schema,
+  Resolver,
   createGraphSchema,
 };
